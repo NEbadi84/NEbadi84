3af9e712d4a940fe3584fa1d0b63d7d354e021a3
Ebadi;Nazanin <29389@Parskhodro.ir>
2022-07-06 04:03:06 -0700

    Second Commit

---
+++ WebAPIProject-master/Entities/User/Role.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.User
+{
+    public class Role:BaseEntity
+    {
+       // [Required]
+       // [StringLength(50)]
+        public string Name { get; set; }
+
+       // [Required]
+       // [StringLength(100)]
+        public string Description { get; set; }
+        
+    }
+}
